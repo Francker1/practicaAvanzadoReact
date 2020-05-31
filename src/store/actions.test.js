6ad7478a3d74279929d40ea1029c06b5935b6454
Italo Franco <italo.ravina@gmail.com>
2020-05-31 16:54:39 +0200

    test: testing asynchronous actions

--- src/store/actions.test.js
+++ src/store/actions.test.js
@@ -1,5 +1,16 @@
+import configureStore from "redux-mock-store";
+import thunk from "redux-thunk";
+
 import * as actions from "./actions";
 import * as TYPES from "./types";
+
+import AdsService from "../services/Ads";
+
+jest.mock("../services/Ads");
+
+const middlewares = [thunk.withExtraArgument({ AdsService })];
+const mockStore = configureStore(middlewares);
+const store = mockStore({});
 
 describe("actions", () => {
   describe("addToFavsSuccess", () => {
@@ -23,6 +34,47 @@ describe("actions", () => {
       };
 
       expect(actions.registerUserSuccess(user)).toEqual(expectedAction);
+    });
+  });
+
+  describe("fetchAds", () => {
+    test("should dispatch FETCH_BIKES_REQUEST and FETCH_BIKES_SUCCESS actions", async () => {
+      const dispatch = jest.fn();
+      const action = actions.fetchAds();
+      const getState = () => {};
+      const ads = [];
+      AdsService.getAllAds.mockResolvedValue(ads);
+
+      await action(dispatch, getState);
+
+      expect(dispatch).toHaveBeenNthCalledWith(1, {
+        type: TYPES.FETCH_ADS_REQUEST,
+      });
+
+      expect(AdsService.getAllAds).toHaveBeenCalled();
+
+      expect(dispatch).toHaveBeenNthCalledWith(2, {
+        type: TYPES.FETCH_ADS_SUCCESS,
+        ads,
+      });
+    });
+  });
+
+  describe("fetchUser", () => {
+    beforeEach(async () => {
+      await store.dispatch(actions.fetchUser());
+    });
+
+    test("should dispatch actions", () => {
+      const user = {
+        username: localStorage.getItem("user"),
+        loggedIn: localStorage.getItem("loggedIn"),
+      };
+      const expectedActions = [
+        { type: TYPES.FETCH_USER_REQUEST },
+        { type: TYPES.FETCH_USER_SUCCESS, user },
+      ];
+      expect(store.getActions()).toEqual(expectedActions);
     });
   });
 });
